57336192fb703418cba25cd5574ac869cb8b345f
Dawid Maroń <daw.maron@gmail.com>
2023-01-05 23:19:39 +0100

    init - update

---
+++ src/main/java/pl/maron/dawid/forum_rc/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package pl.maron.dawid.forum_rc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.maron.dawid.forum_rc")
+public class AppConfiguration {
+}
---
+++ src/main/java/pl/maron/dawid/forum_rc/controllers/CommonController.java
@@ -0,0 +1,13 @@
+package pl.maron.dawid.forum_rc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommonController {
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String main() {
+        return "main";
+    }
+}
