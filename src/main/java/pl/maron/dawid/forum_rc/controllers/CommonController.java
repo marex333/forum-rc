f681f48e545cb0c66d84ba01e6befbd19e33b8cc
Dawid Maroń <daw.maron@gmail.com>
2023-01-06 01:15:19 +0100

    update logo transparent

--- src/main/java/pl/maron/dawid/forum_rc/controllers/CommonController.java
+++ src/main/java/pl/maron/dawid/forum_rc/controllers/CommonController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CommonController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String main() {
+        return "redirect:/";
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main2() {
         return "main";
     }
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping(value = "/faq", method = RequestMethod.GET)
+    public String faq() {
+        return "faq";
+    }
+
 }
